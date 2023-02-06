5ddd4298bd425c813cf7713e179539878a21de85
Mansur Dagaev <mans.4e4en@mail.ru>
2023-02-06 21:27:00 +0300

    4-5

---
+++ 60 task/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+class Program
+{
+static void Main()
+{
+int[,,] matrix3d = new int [2,2,2];
+FillArray(matrix3d);
+PrintArray(matrix3d);
+int element=int.Parse(Console.ReadLine());
+
+void FillArray(int[,,] matrix3d)
+
+{
+
+    for(int i = 0; i < matrix3d.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3d.GetLength(1); j++) 
+         {
+        for (int k = 0; k < matrix3d.GetLength(2); k++) 
+    {
+        matrix3d[i,j,k] = new Random().Next(10,100);
+    }
+        }
+    }
+}
+    
+
+void PrintArray (int[,,] matrix3d)
+{
+  for (int i = 0; i < matrix3d.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix3d.GetLength(1); j++)
+    {
+          for (int k = 0; k < matrix3d.GetLength(2); k++)
+      {
+        Console.Write( $"{matrix3d[i,j,k]}({i},{j},{k})");
+      }
+      Console.WriteLine();
+    }
+        
+       
+} 
+
+      }
+    }
+  }
+}
+
+
+
---
+++ 62 task/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+class Program
+{
+static void Main()
+{
+
+
+int n = 4;
+int [,] matrix = new int [n,n];
+
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+  matrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= matrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > matrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+FillArray(matrix);
+
+void FillArray(int [,] matr)
+{
+
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) 
+    {
+        if (matrix[i,j] / 10 <= 0)
+        Console.Write($" {matrix[i,j]} ");
+
+        else Console.Write($"{matrix[i,j]} ");
+    }
+    Console.WriteLine();
+    }
+}
+}
+}
+}
